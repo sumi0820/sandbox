c0d5ab8d6f6ee0c5bc61b53e27ef9fd23d091796
sumiya_ushiro <u.sumi0820@gmail.com>
2022-04-29 22:41:16 +0900

    Updated for cypress

--- cypress/integration/sample.spec.ts
+++ cypress/integration/sample.spec.ts
@@ -2,11 +2,25 @@ describe('External link', () => {
   it('should link to a valid link', () => {
     cy.visit('http://localhost:3000/');
 
-    cy.get('a[href*="/StoffelFinance"]')
+    // external link1
+    cy.get('[data-test="external-link1"]')
       .should('have.attr', 'target', '_blank')
-      .should('have.attr', 'rel', 'noopener noreferrer');
+      .should('have.attr', 'rel', 'noopener noreferrer')
+      .should('have.attr', 'href')
+      .and('include', 'twitter.com/StoffelFinance');
 
-    cy.get('a[href*="/StoffelFinance"]').then((link) => {
+    cy.get('[data-test="external-link1"]').then((link) => {
+      cy.request(link.prop('href')).its('status').should('eq', 200);
+    });
+
+    // external link2
+    cy.get('[data-test="external-link2"]')
+      .should('have.attr', 'target', '_blank')
+      .should('have.attr', 'rel', 'noopener noreferrer')
+      .should('have.attr', 'href')
+      .and('include', '/docs.stoffelmpc.com');
+
+    cy.get('[data-test="external-link2"]').then((link) => {
       cy.request(link.prop('href')).its('status').should('eq', 200);
     });
   });
@@ -16,7 +30,7 @@ describe('Internal link', () => {
   it('should be navigated to a correct page', () => {
     cy.visit('http://localhost:3000/');
 
-    cy.get('a[href*="/dashboard"]').click();
+    cy.get('[data-test="internal-link1"]').click();
 
     cy.url().should('include', '/dashboard');
 
--- src/components/templates/MainVisual.tsx
+++ src/components/templates/MainVisual.tsx
@@ -55,7 +55,9 @@ const MainVisual: React.FC = () => {
 
   return (
     <div className={classes.container} id="main" ref={logoItem}>
-      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/dashboard" data-test="internal-link1">
+        Dashboard
+      </Link>
       <h1>This is a test - h1</h1>
       <h2>This is a test - h2</h2>
       <h3>This is a test - h3</h3>
@@ -67,6 +69,7 @@ const MainVisual: React.FC = () => {
         href="https://twitter.com/StoffelFinance"
         target="_blank"
         rel="noopener noreferrer"
+        data-test="external-link1"
       >
         This is a test - a
       </a>
@@ -74,6 +77,7 @@ const MainVisual: React.FC = () => {
         href="https://docs.stoffelmpc.com/"
         target="_blank"
         rel="noopener noreferrer"
+        data-test="external-link2"
       >
         This is a test - b
       </a>
